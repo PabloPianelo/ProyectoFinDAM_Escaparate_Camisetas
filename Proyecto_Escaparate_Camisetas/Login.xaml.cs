d3a1fbeb9dea9ef36f46a22b574947bfbeabae46
PabloPianelo <pablopianeloxd@gmail.com>
2023-03-22 17:28:57 +0100

    first commit

---
+++ Proyecto_Escaparate_Camisetas/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Escaparate_Camisetas
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Registro registro = new Registro();
+            registro.Show();
+            this.Close();
+        }
+    }
+}
